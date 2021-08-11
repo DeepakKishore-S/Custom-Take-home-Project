d32586c8866fa982b2b94c6b77d459a83e081b91
DeepakKishore-S <sdkdeepak600@gmail.com>
2021-08-11 23:10:49 +0530

    highlight in menu

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -9,6 +9,9 @@ class Pages extends StatefulWidget {
 
 class _PagesState extends State<Pages> {
   final PageController _pgController = PageController(initialPage: 0);
+  bool pg1 = true;
+  bool pg2 = false;
+  bool pg3 = false;
 
   tap(int pageNo) {
     if (pageNo == 3) {
@@ -17,19 +20,23 @@ class _PagesState extends State<Pages> {
       _pgController.jumpToPage(pageNo);
     }
   }
+
   Widget pageBtn(int pageNo, int currentPage) {
     return Center(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment. spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Text("Page $currentPage"),
-          MaterialButton(elevation: 2,
+          MaterialButton(
+            elevation: 2,
             color: Colors.black,
             onPressed: () {
               tap(currentPage);
             },
-            child: Text("Go to Page $pageNo",
-            style: TextStyle(color: Colors.white),),
+            child: Text(
+              "Go to Page $pageNo",
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ],
       ),
@@ -51,29 +58,53 @@ class _PagesState extends State<Pages> {
               ),
               child: Text(
                 'Menu',
-                style: TextStyle(
-                  fontSize: 26,
-                  color: Colors.white
-                ),
+                style: TextStyle(fontSize: 26, color: Colors.white),
               ),
             ),
             ListTile(
-              title: const Text("Page 1"),
+              tileColor: pg1 ? Colors.black: Colors.white,
+              title:  Text("Page 1",
+              style: TextStyle(
+                color: pg1 ? Colors.white: Colors.black,
+              ),),
               onTap: () {
+                setState(() {
+                  pg1 = false;
+                  pg2 = true;
+                  pg3 = false;
+                });
                 tap(0);
                 Navigator.pop(context);
               },
             ),
             ListTile(
-              title: const Text("Page 2"),
+              tileColor: pg2 ? Colors.black: Colors.white,
+              title: Text("Page 2",
+              style: TextStyle(
+                color: pg2 ? Colors.white: Colors.black,
+              ),),
               onTap: () {
+                setState(() {
+                  pg1 = false;
+                  pg2 = true;
+                  pg3 = false;
+                });
                 tap(1);
                 Navigator.pop(context);
               },
             ),
             ListTile(
-              title: const Text("Page 3"),
+              tileColor: pg3 ? Colors.black: Colors.white,
+              title: Text("Page 3",
+              style: TextStyle(
+                color: pg3 ? Colors.white: Colors.black,
+              ),),
               onTap: () {
+                setState(() {
+                  pg1 = false;
+                  pg2 = false;
+                  pg3 = true;
+                });
                 tap(2);
                 Navigator.pop(context);
               },
@@ -84,13 +115,11 @@ class _PagesState extends State<Pages> {
       body: PageView(
         controller: _pgController,
         children: [
-          pageBtn(2,1),
-          pageBtn(3,2),
-          pageBtn(1,3),
+          pageBtn(2, 1),
+          pageBtn(3, 2),
+          pageBtn(1, 3),
         ],
       ),
     );
   }
-
-  
 }
